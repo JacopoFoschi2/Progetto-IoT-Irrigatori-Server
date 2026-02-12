45995ca2f62d99e49947cb5b7393f0216c5c7b8e
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-12 09:42:50 +0100

    feat: adapted app.ts to new files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
-import sensorsRoutes from "api/router/sensor-routes";
+import sensorsRoutes from "./api/routes/sensor-routes";
+import "./mqtt/mqttClient";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/sensors", sensorsRoutes);
+
+// frontend statico
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
